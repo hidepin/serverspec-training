0ccffb78c23256d9c375d533b52f4dacef601392
vagrant <vagrant@sstarget.(none)>
2016-05-21 15:31:32 +0000

    add template setting.
    テンプレートを読み込み登録する機能を追加

--- Rakefile
+++ Rakefile
@@ -1,22 +1,22 @@
 require 'rake'
 require 'rspec/core/rake_task'
+require 'yaml'
 
-targets = %w(
-  127.0.0.1
-)
+properties = YAML.load_file('properties.yml')
 
 task :spec    => 'spec:all'
 task :default => :spec
 
 namespace :spec do
-  task :all     => targets
+  task :all     => properties.keys.map
   task :default => :all
 
-  targets.each do |target|
+  properties.keys.each do |target|
     desc "Run serverspec tests to #{target}"
 
     RSpec::Core::RakeTask.new(target.to_sym) do |t|
-      ENV['TARGET_HOST'] = target
+      ENV['TARGET_HOST'] = properties[target][:ip]
+      ENV['SERVERNAME'] = properties[target][:servername]
       t.pattern = "spec/default/*_spec.rb"
       t.fail_on_error = false
     end
--- spec/default/sample_spec.rb
+++ spec/default/sample_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'erb'
 
 describe package('httpd'), :if => os[:family] == 'redhat' do
   it { should be_installed }
@@ -12,3 +13,20 @@ end
 describe port(80) do
   it { should be_listening }
 end
+
+describe command('cat /etc/httpd/conf/httpd.conf') do
+  servername = ENV['SERVERNAME']
+  httpd_template = ERB.new(File.read('spec/template/httpd.conf.erb')).result(binding)
+  its(:stdout) { should eq httpd_template }
+end
+
+describe command('rpm -qa | sort') do
+  rpm_list = File.read('spec/template/rpmlist')
+  its(:stdout) { should eq rpm_list }
+end
+
+describe command('chkconfig --list|sort') do
+  chkconfiglist = File.read('spec/template/chkconfiglist')
+  its(:stdout) { should eq chkconfiglist }
+end
+
